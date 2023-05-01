b70e0c5bd57d27b43bad538414472e26f08e8743
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-01 12:38:26 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -23,7 +23,7 @@ function currentTime() {
     
    let time = hh + ":" + mm + ":" + ss + " " + session;
 
-  document.getElementById("clock").innerText = time; 
+  document.getElementsByClassName("clock").innerText = time; 
   let t = setTimeout(function(){ currentTime() }, 1000);
 }
 currentTime();
