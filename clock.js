dda0709e93cbd1b72c2f3aa8115616d4d8524b04
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2026-03-22 18:15:34 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -76,5 +76,5 @@ function changeToWhite() {
 
 function customHex() {
   hex = prompt("Enter Custom Hex Color Code:");
-  document.getElementById("clock").style.textShadow = "0 0 10px ${hex}";
+  document.getElementById("clock").style.textShadow = `0 0 10px ${hex}`;
 }
